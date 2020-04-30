c8e2d6b6b8f0b7d2202394ccfa73a2611bcd9272
Dmitry Poliuha <dm.poliuha@gmail.com>
2020-04-30 15:44:54 +0300

    update helpers

--- swagger2locustio/templates/helpers_templates.py
+++ swagger2locustio/templates/helpers_templates.py
@@ -8,7 +8,8 @@ HELPER_MAPPING.update(dict.fromkeys(["bool", "boolean"], "Helper.get_float_value
 HELPER_MAPPING.update(dict.fromkeys(["null"], "Helper.get_null_value"))
 HELPER_MAPPING.update(dict.fromkeys(["str", "string"], "Helper.get_string_value"))
 
-HELPER_CLASS_TEMPLATE = Template("""import datetime
+HELPER_CLASS_TEMPLATE = Template(
+    """import datetime
 import random
 import string
 
@@ -21,22 +22,23 @@ class Helper:
     @staticmethod
     def get_int_value(start: int = -100, end: int = 100):
         return random.randint(start, end)
-        
-    @staticmethod
-    def get_float_value(start: int = -100, end: int = 100):
-        return random.random() * Helper.get_int_value(start, end)
-        
+
+    @classmethod
+    def get_float_value(cls, start: int = -100, end: int = 100):
+        return random.random() * cls.get_int_value(start, end)
+
     @staticmethod
     def get_bool_value():
         return random.choice([True, False])
-        
+
     @staticmethod
     def get_null_value():
         return None
-        
-    @staticmethod
-    def get_string_value(min_len: int = 0, max_len: int = 100):
-        string_len = Helper.get_int_value(min_len, max_len)
+
+    @classmethod
+    def get_string_value(cls, min_len: int = 0, max_len: int = 100):
+        string_len = cls.get_int_value(min_len, max_len)
         return "".join(random.choices(string.ascii_uppercase + string.digits, k=string_len))
 
-""")
+"""
+)
