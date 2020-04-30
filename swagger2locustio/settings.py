d55d949547b1ff95ec2bfc3733cd8f220c133c96
Oleh Krupko <oleh.krupko@gmail.com>
2020-04-30 14:31:44 +0300

    coloredlogs are now used for log formatting

--- swagger2locustio/settings.py
+++ swagger2locustio/settings.py
@@ -1,6 +1,6 @@
 """Module: This module allows to configure library"""
 
-import logging.config
+import coloredlogs
 
 API_OPERATIONS = ("get", "post", "put", "patch", "delete", "head", "options", "trace")
 LOGGING_LEVELS = ("info", "debug", "warning", "error")
@@ -9,19 +9,4 @@ LOGGING_LEVELS = ("info", "debug", "warning", "error")
 def config_logger(loglevel: str):
     """Function: Configure logger"""
 
-    logging.config.dictConfig(
-        {
-            "version": 1,
-            "disable_existing_loggers": True,
-            "formatters": {"standard": {"format": "%(asctime)s [%(levelname)s] %(filename)s: %(message)s"},},
-            "handlers": {
-                "default": {
-                    "level": loglevel,
-                    "formatter": "standard",
-                    "class": "logging.StreamHandler",
-                    "stream": "ext://sys.stdout",  # Default is stderr
-                },
-            },
-            "loggers": {"": {"handlers": ["default"], "level": loglevel, "propagate": False},},  # root logger
-        }
-    )
+    coloredlogs.install(level=loglevel, fmt="%(asctime)s [%(levelname)s] %(filename)s: %(message)s")
