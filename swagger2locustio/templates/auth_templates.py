e75773bed83f8fe743bdbfe1a2dce1be357d22e0
Dmitry Poliuha <dm.poliuha@gmail.com>
2020-05-04 15:58:46 +0300

    move auth templates to separate file

---
+++ swagger2locustio/templates/auth_templates.py
@@ -0,0 +1,19 @@
+"""Module: Auth templates"""
+
+from jinja2 import Template
+
+AUTH_BASIC_TEMPLATE = Template(
+    """
+        auth_str = str(os.getenv(\"TEST_USER_LOGIN\")) + ":" + str(os.getenv(\"TEST_USER_PASSWORD\"))
+        credentials = b64encode(auth_str.encode()).decode("utf-8")
+        credentials = "Basic " + credentials
+        self.client.headers.update({"Authorization": credentials})
+"""
+)
+
+
+AUTH_KEY_HEADER_TEMPLATE = Template(
+    """
+        self.client.headers.update({"{{ name }}": str(os.getenv(\"TEST_USER_API_KEY\"))})
+"""
+)
--- swagger2locustio/templates/locustfile_templates.py
+++ swagger2locustio/templates/locustfile_templates.py
@@ -1,4 +1,4 @@
-"""Module: Swagger Base Parser"""
+"""Module: locustfile templates"""
 
 from jinja2 import Template
 
@@ -45,23 +45,6 @@ FUNC_TEMPLATE = Template(
             cookies={{ cookie_params }},
         )
 
-"""
-)
-
-
-AUTH_BASIC_TEMPLATE = Template(
-    """
-        auth_str = str(os.getenv(\"TEST_USER_LOGIN\")) + ":" + str(os.getenv(\"TEST_USER_PASSWORD\"))
-        credentials = b64encode(auth_str.encode()).decode("utf-8")
-        credentials = "Basic " + credentials
-        self.client.headers.update({"Authorization": credentials})
-"""
-)
-
-
-AUTH_KEY_HEADER_TEMPLATE = Template(
-    """
-        self.client.headers.update({"{{ name }}": str(os.getenv(\"TEST_USER_API_KEY\"))})
 """
 )
 
