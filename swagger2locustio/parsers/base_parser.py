3950570377c22ebb13cdc4c983e374731e251749
Oleh Krupko <oleh.krupko@gmail.com>
2020-05-06 15:11:25 +0300

    parsers: common logic is in base_parser

--- swagger2locustio/parsers/base_parser.py
+++ swagger2locustio/parsers/base_parser.py
@@ -1,6 +1,7 @@
 """Module: Base Parser"""
 
-from abc import ABC, abstractmethod
+from abc import ABC
+from copy import deepcopy
 from typing import Set, Dict
 
 
@@ -14,26 +15,67 @@ class SwaggerBaseParser(ABC):
             "host": self.parse_host_data(file_content),
             "security": self.parse_security_data(file_content),
             "paths": self.parse_paths_data(file_content, mask),
-            "definitions": self.parse_definitions(file_content),
         }
         return data
 
-    @abstractmethod
-    def parse_host_data(self, file_content: dict) -> str:
+    @staticmethod
+    def parse_host_data(file_content: dict) -> str:
         """Method: parse host data"""
-        raise NotImplementedError
 
-    @abstractmethod
-    def parse_security_data(self, file_content: dict) -> dict:
+        return file_content.get("host", "")
+
+    @staticmethod
+    def parse_security_data(file_content: dict) -> dict:
         """Method: parse security data"""
-        raise NotImplementedError
 
-    @abstractmethod
+        security = {}
+        security_definitions = file_content.get("securityDefinitions", {})
+        for security_mode, security_config in security_definitions.items():
+            if security_config["type"] in ("BasicAuth", "apiKey"):
+                security[security_mode] = security_config
+            else:
+                raise ValueError("Security type %s is not supported" % security_config["type"])
+        return security
+
     def parse_paths_data(self, file_content: dict, mask: Dict[str, Set[str]]) -> dict:
         """Method: parse paths data"""
-        raise NotImplementedError
 
-    @abstractmethod
-    def parse_definitions(self, file_content: dict) -> dict:
-        """Method: parse definitions data"""
-        raise NotImplementedError
+        paths_white_list = mask["paths_white_list"]
+        paths_black_list = mask["paths_black_list"]
+        tags_white_list = mask["tags_white_list"]
+        tags_black_list = mask["tags_black_list"]
+
+        api_paths = {}
+        paths = file_content.get("paths")
+        if paths is None:
+            raise ValueError("No paths is found in swagger file")
+        for path, path_data in paths.items():
+            valid_path_methods = {}
+            if (paths_white_list and path.lower() not in paths_white_list) or (path.lower() in paths_black_list):
+                continue
+
+            for path_method, method_data in path_data.items():
+                if path_method.lower() not in mask["operations_white_list"]:
+                    continue
+                tags = set(tag.lower() for tag in method_data.get("tags", []))
+                if tags_white_list and not tags_white_list.intersection(tags) or tags_black_list.intersection(tags):
+                    continue
+                valid_path_methods[path_method] = {
+                    "params": self._parse_params(method_data.get("parameters", [])),
+                    "responses": method_data.get("responses", {}),
+                }
+            api_paths[path] = valid_path_methods
+        return api_paths
+
+    @staticmethod
+    def _parse_params(params: dict) -> dict:
+        param_data = {}
+        for param in params:
+            param_name = param.get("name")
+            # if not param_name or not param.get("default") or not param.get("in"):
+            if not param_name or not param.get("in"):
+                if param.get("required"):
+                    raise ValueError("Not full info about required param")
+                continue
+            param_data[param_name] = deepcopy(param)
+        return param_data
